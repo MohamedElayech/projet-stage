ab79dc17d5751da00cfc9004b7c2c08df9a6d832
Sarradziri <sarradziri28@gmail.com>
2024-09-10 21:01:24 +0100

    2

--- Client/src/Pages/Usefulness/usefulness_inputs.jsx
+++ Client/src/Pages/Usefulness/usefulness_inputs.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import './HomePage2.css'
+import './usefulness_inputs.css'
 import ResultComponent from './ResultComponent';
 
 
